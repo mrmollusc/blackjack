762059ba25a7cc9721cbc7464cbd0682391b39f6
mrmollusc <rayscambo@gmail.com>
2026-05-12 22:51:24 +1000

    Add files via upload

---
+++ blackjack.js
@@ -0,0 +1,60 @@
+const deck = [
+    1,2,3,4,5,6,7,8,9,10,10,10,10, //suite 1
+    1,2,3,4,5,6,7,8,9,10,10,10,10, //suite 2
+    1,2,3,4,5,6,7,8,9,10,10,10,10, //suite 3
+    1,2,3,4,5,6,7,8,9,10,10,10,10, //suite 4
+]
+const namedeck = [
+  "Ace of Hearts", "2 of Hearts", "3 of Hearts", "4 of Hearts", "5 of Hearts", "6 of Hearts", "7 of Hearts", "8 of Hearts", "9 of Hearts", "10 of Hearts", "Jack of Hearts", "Queen of Hearts", "King of Hearts",
+  "Ace of Diamonds", "2 of Diamonds", "3 of Diamonds", "4 of Diamonds", "5 of Diamonds", "6 of Diamonds", "7 of Diamonds", "8 of Diamonds", "9 of Diamonds", "10 of Diamonds", "Jack of Diamonds", "Queen of Diamonds", "King of Diamonds",
+  "Ace of Clubs", "2 of Clubs", "3 of Clubs", "4 of Clubs", "5 of Clubs", "6 of Clubs", "7 of Clubs", "8 of Clubs", "9 of Clubs", "10 of Clubs", "Jack of Clubs", "Queen of Clubs", "King of Clubs",
+  "Ace of Spades", "2 of Spades", "3 of Spades", "4 of Spades", "5 of Spades", "6 of Spades", "7 of Spades", "8 of Spades", "9 of Spades", "10 of Spades", "Jack of Spades", "Queen of Spades", "King of Spades"
+];
+
+//variables
+let playertotal = 0;
+let aitotal = 0;
+let carddrawn = 0;
+let playerturn = true;
+
+//funcs
+function drawcard(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
+console.log('Welcome to CMDline Blackjack v 1.0!')
+console.log('For now, a few things have not been implemented: Ace values, Splitting, 2 Player Mode')
+console.log('A Changelog will appear below, ticking off the things implemented :)')
+console.log('-mrmollusc')
+//console.log('Click Enter To Begin')
+while(true){
+    let deal = prompt('Hit? (Y/N)')
+    if(deal==='Y' && playertotal<21){
+        
+        let randcard = drawcard(1,52);
+        carddraw=deck[randcard];
+        console.log('You drew', namedeck[randcard], 'giving you', deck[randcard]);
+        playertotal+=deck[randcard];
+        console.log('Your total is:', playertotal);
+
+        if(playertotal>21){
+            console.log('Bust!')
+            break;
+        }
+        else if(playertotal==21){
+            console.log('Blackjack!')
+            break;
+    }
+
+    }
+    else if(deal==='N' && playertotal<21){
+        playerturn = false;
+        console.log('Your total is:', playertotal);
+        console.log("Dealer's Turn...");
+        break;
+    }
+    
+    
+}
